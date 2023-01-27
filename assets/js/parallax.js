44257c77710999afcfb292806d74c09521cf3c8f
lucasrenandns <lucasrenandns96@gmail.com>
2023-01-27 09:53:17 -0300

    Parallax Scrolling

---
+++ assets/js/parallax.js
@@ -0,0 +1,11 @@
+const firstImage = document.getElementById("first-image")
+const lastImage = document.getElementById("last-image")
+const title = document.getElementById("title")
+
+window.addEventListener("scroll", function() {
+    const scrollValue = window.scrollY
+
+    firstImage.style.top = scrollValue * 0.7 + "px" 
+    lastImage.style.bottom = scrollValue * 0.5 + "px" 
+    title.style.top = scrollValue * 1 + "px" 
+})
